cba6e07e7643cbc9bb8b1bb559996c7838aa6da3
Supun Kandaudahewa <skandaudahewa@gmail.com>
2026-09-16 11:29:44 -0400

    chore: removed the local connection string

--- SupportHub.Infrastructure/DesignTimeDbContextFactory.cs
+++ SupportHub.Infrastructure/DesignTimeDbContextFactory.cs
@@ -15,7 +15,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<SupportHub
         var optionsBuilder = new DbContextOptionsBuilder<SupportHubDbContext>();
 
         var connectionString = Environment.GetEnvironmentVariable("SUPPORTHUB_MIGRATIONS_CONNECTION")
-            ?? "Server=Armoury;Database=SupportHubCore;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+            ?? "{local connection string}";
 
         optionsBuilder.UseSqlServer(connectionString);
 
